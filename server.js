a2c4e68c42ff90ba6e61117363a754e75b3e98df
Edgar Andres Flores <edgarandres.flores@globant.com>
2020-09-09 13:59:28 -0300

    nuevo commit

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var app = express();
 
 app.get('/', (req, res) => {
 	res.send('Hello World');
-})
+});
+
+app.get('/about' (req, res) => {
+	res.send('<h1>CloudOps-Academy</h1>');
+});
 
 app.listen(8080, () => {
 	console.log('llego una request');
